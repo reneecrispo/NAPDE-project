0cba0d6a73b0793fa45f7bd71dadf6e3f5e31df1
reneecrispo <renee.crispo@gmail.com>
2024-07-23 21:56:54 +0200

    addpath changes

--- add_path.m
+++ add_path.m
@@ -1,5 +1,4 @@
 addpath (genpath ('nurbs-1.4.3'))
 addpath (genpath ('geopdes'))
 addpath (genpath ('geopdes_hierarchical'))
-
-addpath (genpath ('Esercizi_Dede+nostri_riassunti'))
+addpath (genpath('display_functions'))
